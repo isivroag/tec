4780ee1453e18a71488bebd9b41bca87ad9f86d6
isivroag <isivroag@gmail.com>
2021-02-04 17:26:52 -0600

    IVAN-logo

--- templates/navegacion.php
+++ templates/navegacion.php
@@ -3,7 +3,7 @@
 
   <a href="inicio.php" class="brand-link">
 
-    <img src="img/logo.png" alt="Tecniem Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <img src="img/mundo.jpg" alt="Tecniem Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
     <span class="brand-text font-weight-bold">TECNIEM</span>
   </a>
 
