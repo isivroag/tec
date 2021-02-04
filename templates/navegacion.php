395a8eed4ad6e73027d45327e7fb04ed8fb49e00
isivroag <isivroag@gmail.com>
2021-02-04 17:14:26 -0600

    Primer cambio O

--- templates/navegacion.php
+++ templates/navegacion.php
@@ -3,8 +3,8 @@
 
   <a href="inicio.php" class="brand-link">
 
-    <img src="img/logo.png" alt="Gallery Stone Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-    <span class="brand-text font-weight-bold">Grupo Aquax</span>
+    <img src="img/logo.png" alt="Tecniem Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <span class="brand-text font-weight-bold">Tecniem</span>
   </a>
 
   <!-- Sidebar -->
