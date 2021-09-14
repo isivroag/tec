1938dac092848813c3457740a72514d3e3f7a62e
isivroag <isivroag@gmail.com>
2021-09-13 19:22:51 -0500

    cambio en conexion

--- bd/conexion.php
+++ bd/conexion.php
@@ -6,7 +6,7 @@
             define('servidor','tecniem.com');
             define('bd_nombre','tecniemc_tecniem');
             define('usuario','tecniemc_ivan');
-            define('password','Sistemas.2021');
+            define('password','66obispo.colima');
 
             $opciones=array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
 
